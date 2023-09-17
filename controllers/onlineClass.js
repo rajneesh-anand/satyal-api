07c45b1228fe7b7a0411097a12e0689140fd5e09
milan aryal <milanaryal74@gmail.com>
2023-09-17 19:21:46 +0545

    online class api have been updated

--- controllers/onlineClass.js
+++ controllers/onlineClass.js
@@ -3,7 +3,7 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 // Create a new online class
-exports.createClass = async (req, res) => {
+let createClass = async (req, res) => {
   try {
     // Extract data from the request body
     const { onlineClassName, onlineClassGrade, onlineClassSection, teacherId } = req.body;
@@ -30,74 +30,76 @@ exports.createClass = async (req, res) => {
 };
 
 // Enroll a student in an online class
-exports.enrollClass = async (req, res) => {
-  try {
-    // Extract data from the request body
-    const { enrollmentCode, studentId } = req.body;
+// exports.enrollClass = async (req, res) => {
+//   try {
+//     // Extract data from the request body
+//     const { enrollmentCode, studentId } = req.body;
 
-    // Find the class with the given enrollment code
-    const onlineClass = await prisma.onlineClass.findUnique({
-      where: { enrollmentCode },
-    });
+//     // Find the class with the given enrollment code
+//     const onlineClass = await prisma.onlineClass.findUnique({
+//       where: { enrollmentCode },
+//     });
 
-    if (!onlineClass) {
-      return res.status(404).json({ error: 'Class not found' });
-    }
+//     if (!onlineClass) {
+//       return res.status(404).json({ error: 'Class not found' });
+//     }
 
-    // Connect the student to the class
-    await prisma.onlineClass.update({
-      where: { enrollmentCode },
-      data: {
-        students: { connect: { id: studentId } },
-      },
-    });
+//     // Connect the student to the class
+//     await prisma.onlineClass.update({
+//       where: { enrollmentCode },
+//       data: {
+//         students: { connect: { id: studentId } },
+//       },
+//     });
 
-    res.status(200).json({ message: 'Enrolled in class successfully' });
-  } catch (error) {
-    console.error('Error enrolling in class:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+//     res.status(200).json({ message: 'Enrolled in class successfully' });
+//   } catch (error) {
+//     console.error('Error enrolling in class:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// };
 
 // Get details of an online class
-exports.getClassDetails = async (req, res) => {
-  try {
-    // Extract the classId from the request parameters
-    const classId = parseInt(req.params.classId);
+// exports.getClassDetails = async (req, res) => {
+//   try {
+//     // Extract the classId from the request parameters
+//     const classId = parseInt(req.params.classId);
 
-    // Fetch class details
-    const onlineClass = await prisma.onlineClass.findUnique({
-      where: { id: classId },
-      include: {
-        teacher: true,
-        students: true,
-        notes: true,
-        worksheets: true,
-      },
-    });
+//     // Fetch class details
+//     const onlineClass = await prisma.onlineClass.findUnique({
+//       where: { id: classId },
+//       include: {
+//         teacher: true,
+//         students: true,
+//         notes: true,
+//         worksheets: true,
+//       },
+//     });
 
-    if (!onlineClass) {
-      return res.status(404).json({ error: 'Class not found' });
-    }
+//     if (!onlineClass) {
+//       return res.status(404).json({ error: 'Class not found' });
+//     }
 
-    res.status(200).json(onlineClass);
-  } catch (error) {
-    console.error('Error fetching class details:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+//     res.status(200).json(onlineClass);
+//   } catch (error) {
+//     console.error('Error fetching class details:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// };
 
 // Add a note to an online class
-exports.addNote = async (req, res) => {
-  // Implementation for adding notes to the class
-};
+// exports.addNote = async (req, res) => {
+//   // Implementation for adding notes to the class
+// };
 
 // Add a worksheet to an online class
-exports.addWorksheet = async (req, res) => {
-  // Implementation for adding worksheets to the class
-};
+// exports.addWorksheet = async (req, res) => {
+//   // Implementation for adding worksheets to the class
+// };
 
 // Utility function to generate a random enrollment code
-function generateEnrollmentCode() {
-  // Implementation for generating a random code (e.g., a combination of letters and numbers)
-}
+// function generateEnrollmentCode() {
+//   // Implementation for generating a random code (e.g., a combination of letters and numbers)
+// }
+
+module.exports={createClass};
--- routes/onlineClass.js
+++ routes/onlineClass.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // Import necessary controllers or services
-const onlineClass = require('./onlineClass'); // You'll create this file
-
+// const onlineClass = require('./onlineClass'); // You'll create this file
+const {createClass} =require('../controllers/onlineClass');
 // Define your routes
-router.post('/create', onlineClass.createClass);
-router.post('/enroll', onlineClass.enrollClass);
-router.get('/details/:classId', onlineClass.getClassDetails);
-router.post('/add-note/:classId', onlineClass.addNote);
-router.post('/add-worksheet/:classId', onlineClass.addWorksheet);
+router.post('/create', createClass);
+// router.post('/enroll', onlineClass.enrollClass);
+// router.get('/details/:classId', onlineClass.getClassDetails);
+// router.post('/add-note/:classId', onlineClass.addNote);
+// router.post('/add-worksheet/:classId', onlineClass.addWorksheet);
 
 module.exports = router;
