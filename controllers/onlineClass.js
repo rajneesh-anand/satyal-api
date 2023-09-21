ff42c86035298d2bd7af01b0cca66651524374fb
Prasanna Koirala <prasanna2koirala@gmail.com>
2023-09-21 11:57:33 +0545

    getting classes as per student and teacher email routes written

--- controllers/onlineClass.js
+++ controllers/onlineClass.js
@@ -48,19 +48,47 @@ exports.createClass = async (req, res) => {
   }
 };
 
+// Enroll a student in an online class
+// exports.enrollClass = async (req, res) => {
+//   try {
+//     // Extract data from the request body
+//     const { enrollCode, studentId } = req.body;
+
+//     // Find the class with the given enrollment code
+//     const onlineClass = await prisma.onlineClass.findUnique({
+//       where: { enrollCode },
+//     });
+
+//     if (!onlineClass) {
+//       return res.status(404).json({ error: 'Class not found' });
+//     }
+
+//     // Connect the student to the class
+//     await prisma.onlineClass.update({
+//       where: { enrollCode },
+//       data: {
+//         students: { connect: { id: studentId } },
+//       },
+//     });
+
+//     res.status(200).json({ message: 'Enrolled in class successfully' });
+//   } catch (error) {
+//     console.error('Error enrolling in class:', error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// };
+
 // Get all online classes based on the teacherEmail
-exports.getAllClassesCreated = async (req, res) => {
+exports.getAllCreatedClasses = async (req, res) => {
   try {
     // Extract the teacherEmail from the request parameters
 
-    const teacherEmail = req.params.teacherEmail;
+    const teacherEmail = req.params.email;
 
     // Fetch all online classes for the teacher
     const onlineClasses = await prisma.onlineClass.findMany({
-      where: { teacher: { email: teacherEmail } },
+      where: { teacherEmail: teacherEmail },
       include: {
-        teacher: true,
-        students: true,
         notes: true,
         worksheets: true,
       },
@@ -69,6 +97,56 @@ exports.getAllClassesCreated = async (req, res) => {
     res.status(200).json(onlineClasses);
   } catch (error) {
     console.error('Error fetching teacher classes:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Get all online classes based on the studentEmail
+exports.getAllEnrolledClasses = async (req, res) => {
+  try {
+    // Extract the studentEmail from the request parameters
+    const studentEmail = req.params.studentEmail;
+
+    // Fetch all online classes for the student
+    const onlineClasses = await prisma.onlineClass.findMany({
+      where: { students: { some: { email: studentEmail } } },
+      include: {
+        notes: true,
+        worksheets: true,
+      },
+    });
+
+    res.status(200).json(onlineClasses);
+  } catch (error) {
+    console.error('Error fetching student classes:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Get details of a particular online class
+exports.getClassDetails = async (req, res) => {
+  try {
+    // Extract the classId from the request parameters
+    const classId = parseInt(req.params.classId);
+
+    // Fetch class details
+    const onlineClass = await prisma.onlineClass.findUnique({
+      where: { id: classId },
+      include: {
+        teacher: true,
+        students: true,
+        notes: true,
+        worksheets: true,
+      },
+    });
+
+    if (!onlineClass) {
+      return res.status(404).json({ error: 'Class not found' });
+    }
+
+    res.status(200).json(onlineClass);
+  } catch (error) {
+    console.error('Error fetching class details:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
@@ -97,88 +175,6 @@ exports.updateMeetingLink = async (req, res) => {
       .json({ message: 'Meeting link updated successfully', updatedOnlineClass });
   } catch (error) {
     console.error('Error updating meeting link:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
-// Enroll a student in an online class
-exports.enrollClass = async (req, res) => {
-  try {
-    // Extract data from the request body
-    const { enrollCode, studentId } = req.body;
-
-    // Find the class with the given enrollment code
-    const onlineClass = await prisma.onlineClass.findUnique({
-      where: { enrollCode },
-    });
-
-    if (!onlineClass) {
-      return res.status(404).json({ error: 'Class not found' });
-    }
-
-    // Connect the student to the class
-    await prisma.onlineClass.update({
-      where: { enrollCode },
-      data: {
-        students: { connect: { id: studentId } },
-      },
-    });
-
-    res.status(200).json({ message: 'Enrolled in class successfully' });
-  } catch (error) {
-    console.error('Error enrolling in class:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
-// Get all online classes based on the studentEmail
-exports.getAllClassesEnrolled = async (req, res) => {
-  try {
-    // Extract the studentEmail from the request parameters
-    const studentEmail = req.params.studentEmail;
-
-    // Fetch all online classes for the student
-    const onlineClasses = await prisma.onlineClass.findMany({
-      where: { students: { some: { email: studentEmail } } },
-      include: {
-        teacher: true,
-        students: true,
-        notes: true,
-        worksheets: true,
-      },
-    });
-
-    res.status(200).json(onlineClasses);
-  } catch (error) {
-    console.error('Error fetching student classes:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
-// Get details of an online class
-exports.getClassDetails = async (req, res) => {
-  try {
-    // Extract the classId from the request parameters
-    const classId = parseInt(req.params.classId);
-
-    // Fetch class details
-    const onlineClass = await prisma.onlineClass.findUnique({
-      where: { id: classId },
-      include: {
-        teacher: true,
-        students: true,
-        notes: true,
-        worksheets: true,
-      },
-    });
-
-    if (!onlineClass) {
-      return res.status(404).json({ error: 'Class not found' });
-    }
-
-    res.status(200).json(onlineClass);
-  } catch (error) {
-    console.error('Error fetching class details:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
--- routes/onlineClass.js
+++ routes/onlineClass.js
@@ -3,18 +3,19 @@ const router = express.Router();
 
 const {
   createClass,
-  enrollClass,
+  //   enrollClass,
   getClassDetails,
-  getAllClassesCreated,
-  getAllClassesEnrolled,
+  getAllCreatedClasses,
+  getAllEnrolledClasses,
   updateMeetingLink,
 } = require('../controllers/onlineClass');
 // Define your routes
 router.post('/create', createClass);
-router.post('/enroll', enrollClass);
+// router.post('/enroll', enrollClass);
 router.get('/details/:classId', getClassDetails);
-router.get('/details', getAllClassesCreated);
-router.get('/details', getAllClassesEnrolled);
+router.get('/createdClasses/:email', getAllCreatedClasses);
+router.get('/enrolledClasses/:email', getAllEnrolledClasses);
+router.patch('/updateLink', updateMeetingLink);
 // router.post('/add-note/:classId', onlineClass.addNote);
 // router.post('/add-worksheet/:classId', onlineClass.addWorksheet);
 
