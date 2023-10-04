09a649d22de3e4c82df795bbe142b3fbba1c23e2
Prasanna Koirala <prasanna2koirala@gmail.com>
2023-10-04 20:08:37 +0545

    normal

--- routes/note.js
+++ routes/note.js
@@ -1,14 +1,11 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
+const router = express.Router();
 
-const {addNoteAtOnlineClass} =require('../controllers/note');
+const { addNoteAtOnlineClass } = require('../controllers/note');
 
-router.post('/onlineClass/add',addNoteAtOnlineClass);
-router.get('/',(req,res)=>{
-    res.status(200).type('json').json('online class get route ')
-})
+router.post('/onlineClass/add', addNoteAtOnlineClass);
+router.get('/', (req, res) => {
+  res.status(200).type('json').json('online class get route ');
+});
 
-
-
-
-module.exports=router;
+module.exports = router;
