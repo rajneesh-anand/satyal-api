5cf630dbd49512ef0178dae908ca20df15adff50
Prasanna Koirala <prasanna2koirala@gmail.com>
2023-10-10 18:22:53 +0545

    formatted nicely by prettier

--- controllers/note.js
+++ controllers/note.js
@@ -42,58 +42,63 @@ exports.addNoteAtOnlineClass = async (req, res) => {
 };
 
 // delete note from online class
-exports.deleteNoteFromOnlineClass=async (req, res) => {
-  try{
-      let {id,onlineClassId,teacherEmail} = req.body;
-   
-      if(!id || !onlineClassId || !teacherEmail){
-        return(
-        res.status(400).type('json').json({error:"Bad Request, Please Try Again"}))
-      }
+exports.deleteNoteFromOnlineClass = async (req, res) => {
+  try {
+    let { id, onlineClassId, teacherEmail } = req.body;
 
-      // searching the onlineclass
-      let onlineClassResponse = await prisma.onlineClass.findUnique({
-        where:{
-          id : onlineClassId
-        }
-      })
-      // authenticating the onlineclass present or not
-      if(!onlineClassResponse){
-        return res.status(400).type('json').json({error:"Bad Request, Please Try Again"})
-      }
-      // authenticating for authorize user
-      if(onlineClassResponse?.teacherEmail !== teacherEmail){
-        return res.status(401).type('json').json({error:"!You Are An Unauthorized User"})
-      }
-      // searching the notice 
-      let onlineClassNoticeResponse = await prisma.note.findUnique({
-        where:{
-          id : id
-        }
-      })
+    if (!id || !onlineClassId || !teacherEmail) {
+      return res
+        .status(400)
+        .type('json')
+        .json({ error: 'Bad Request, Please Try Again' });
+    }
 
-      if(!onlineClassNoticeResponse){
-        return res.status(404).type('json').json({error: "This Notice Do Not Found"})
-      }
-      // deleting notice from DB
-      let onlineClassNoticeDeleteResponse = await prisma.note.delete({
-        where:{
-          id : id
-        }
-      })
-      // if notice does not deleted
-      if(!onlineClassNoticeDeleteResponse){
-         return res.status(500).type('json').json({error: "This Notice Does Not Delete, Please Try Again"})
-      }  
+    // searching the onlineclass
+    let onlineClassResponse = await prisma.onlineClass.findUnique({
+      where: {
+        id: onlineClassId,
+      },
+    });
+    // authenticating the onlineclass present or not
+    if (!onlineClassResponse) {
+      return res
+        .status(400)
+        .type('json')
+        .json({ error: 'Bad Request, Please Try Again' });
+    }
+    // authenticating for authorize user
+    if (onlineClassResponse?.teacherEmail !== teacherEmail) {
+      return res
+        .status(401)
+        .type('json')
+        .json({ error: '!You Are An Unauthorized User' });
+    }
+    // searching the notice
+    let onlineClassNoticeResponse = await prisma.note.findUnique({
+      where: {
+        id: id,
+      },
+    });
 
-      return(
-        res.status(204).type('json')
-        .json({message : "Notice Is Deleted"})
-      )  
-  }catch(err){
-    res
-    .status(500)
-    .type('json')
-    .json({error:"Internel Server Error"})
+    if (!onlineClassNoticeResponse) {
+      return res.status(404).type('json').json({ error: 'This Notice Do Not Found' });
+    }
+    // deleting notice from DB
+    let onlineClassNoticeDeleteResponse = await prisma.note.delete({
+      where: {
+        id: id,
+      },
+    });
+    // if notice does not deleted
+    if (!onlineClassNoticeDeleteResponse) {
+      return res
+        .status(500)
+        .type('json')
+        .json({ error: 'This Notice Does Not Delete, Please Try Again' });
+    }
+
+    return res.status(204).type('json').json({ message: 'Notice Is Deleted' });
+  } catch (err) {
+    res.status(500).type('json').json({ error: 'Internel Server Error' });
   }
-}
+};
