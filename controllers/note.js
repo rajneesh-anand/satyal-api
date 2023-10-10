26024834001736eb0fba3c194973630c81664842
milan aryal <milanaryal74@gmail.com>
2023-10-10 11:46:55 +0545

    onlineclass delete notice function is added

--- controllers/note.js
+++ controllers/note.js
@@ -40,3 +40,60 @@ exports.addNoteAtOnlineClass = async (req, res) => {
     res.status(200).type('json').json('internel server error');
   }
 };
+
+// delete note from online class
+exports.deleteNoteFromOnlineClass=async (req, res) => {
+  try{
+      let {id,onlineClassId,teacherEmail} = req.body;
+   
+      if(!id || !onlineClassId || !teacherEmail){
+        return(
+        res.status(400).type('json').json({error:"Bad Request, Please Try Again"}))
+      }
+
+      // searching the onlineclass
+      let onlineClassResponse = await prisma.onlineClass.findUnique({
+        where:{
+          id : onlineClassId
+        }
+      })
+      // authenticating the onlineclass present or not
+      if(!onlineClassResponse){
+        return res.status(400).type('json').json({error:"Bad Request, Please Try Again"})
+      }
+      // authenticating for authorize user
+      if(onlineClassResponse?.teacherEmail !== teacherEmail){
+        return res.status(401).type('json').json({error:"!You Are An Unauthorized User"})
+      }
+      // searching the notice 
+      let onlineClassNoticeResponse = await prisma.note.findUnique({
+        where:{
+          id : id
+        }
+      })
+
+      if(!onlineClassNoticeResponse){
+        return res.status(404).type('json').json({error: "This Notice Do Not Found"})
+      }
+      // deleting notice from DB
+      let onlineClassNoticeDeleteResponse = await prisma.note.delete({
+        where:{
+          id : id
+        }
+      })
+      // if notice does not deleted
+      if(!onlineClassNoticeDeleteResponse){
+         return res.status(500).type('json').json({error: "This Notice Does Not Delete, Please Try Again"})
+      }  
+
+      return(
+        res.status(204).type('json')
+        .json({message : "Notice Is Deleted"})
+      )  
+  }catch(err){
+    res
+    .status(500)
+    .type('json')
+    .json({error:"Internel Server Error"})
+  }
+}
