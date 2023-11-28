b5916d61a078aa0555a4818a1023965391f03163
Prasanna Koirala <prasanna2koirala@gmail.com>
2023-11-28 13:35:34 +0545

    pulled

--- helper/user-validator.js
+++ helper/user-validator.js
@@ -1,12 +1,26 @@
-const { body } = require("express-validator");
+const { body, validationResult } = require('express-validator');
 
 exports.userSignupValidator = () => {
   return [
     // Validate email format
-    body("email").isEmail().withMessage("Must be a valid email address."),
+    body('email').isEmail().withMessage('Must be a valid email address.'),
     // Validate password length
-    body("password")
+    body('password')
       .isLength({ min: 6 })
-      .withMessage("Password must be at least 6 characters long."),
+      .withMessage('Password must be at least 6 characters long.'),
   ];
+};
+
+exports.validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+
+  const extractedErrors = [];
+  errors.array().map((err) => extractedErrors.push({ [err.param]: err.msg }));
+
+  return res.status(422).json({
+    errors: extractedErrors,
+  });
 };
--- routes/payment.js
+++ routes/payment.js
@@ -1,13 +1,23 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const prisma = require("../lib/prisma");
-const { hashSync, genSaltSync } = require("bcrypt");
-const axios = require("axios");
-const emailMailer = require("../helper/email");
+const prisma = require('../lib/prisma');
+const { hashSync, genSaltSync } = require('bcrypt');
+const axios = require('axios');
+const emailMailer = require('../helper/email');
 
-router.post("/khalti", async (req, res) => {
-  const { userData, payment, userType } = req.body;
- 
+// To give to front
+const subscriptionPlans = [
+  { name: 'Basic', price: 1000 },
+  { name: 'Standard', price: 2000 },
+  { name: 'Premium', price: 3000 },
+  { name: 'Enterprise', price: 4000 },
+];
+
+router.post('/khalti', async (req, res) => {
+  const { userData, payment, selectedPlan, userType } = req.body;
+  console.log("This is user's data:", userData);
+  console.log("This is plan's data:", selectedPlan);
+  console.log("This is user's type:", userType);
 
   let emailExist = await prisma.user.count({
     where: {
@@ -17,7 +27,7 @@ router.post("/khalti", async (req, res) => {
 
   if (emailExist > 0) {
     return res.status(403).json({
-      message: "The email address is already registered !",
+      message: 'The email address is already registered !',
     });
   }
 
@@ -28,10 +38,10 @@ router.post("/khalti", async (req, res) => {
       data: {
         email: userData.email,
         firstName: userData.firstName,
-        middleName:userData?.middleName,
+        middleName: userData?.middleName,
         lastName: userData.lastName,
         parentName: userData.parentName,
-        parentContactNumber:userData.parentContactNumber,
+        parentContactNumber: userData.parentContactNumber,
         password: hashedPassword,
         address: userData.address,
         city: userData.city,
@@ -39,53 +49,61 @@ router.post("/khalti", async (req, res) => {
         studentClass: JSON.stringify(userData.studentClass),
         userContactNumber: userData.userContactNumber,
         userType: userType,
-        userStatus: "Active",
-        kycStatus: userType === "Teacher" ? "Kyc Pending" : "Not Required",
+        userStatus: 'Active',
+        kycStatus: userType === 'Teacher' ? 'Kyc Pending' : 'Not Required',
       },
     });
 
     if (result) {
       const { data } = await axios.post(
-        "https://a.khalti.com/api/v2/epayment/initiate/",
+        'https://khalti.com/api/v2/epayment/initiate/',
         JSON.stringify({
-          return_url: "http://localhost:3000/payment/status",
-          website_url: "http://localhost:3000",
-          amount: 1300,
-          purchase_order_id: "test15022023",
-          purchase_order_name: "test_order_name",
+          return_url: 'http://localhost:3000/payment/status',
+          website_url: 'http://localhost:3000',
+          amount: selectedPlan.price,
+          purchase_order_id: 'Ordewr01',
+          purchase_order_name: 'Basic Plan',
           customer_info: {
-            name: "Ashim Upadhaya",
-            email: "example@gmail.com",
-            phone: "9811496763",
+            name: userData.middleName
+              ? `${userData.firstName} ${userData.middleName} ${userData.lastName}`
+              : `${userData.firstName} ${userData.lastName}`,
+            email: userData.email,
+            phone: userData.userContactNumber,
           },
           amount_breakdown: [
             {
-              label: "Mark Price",
-              amount: 1000,
+              label: 'Mark Price',
+              amount: selectedPlan.price - selectedPlan.price * 0.13,
             },
             {
-              label: "VAT",
-              amount: 300,
+              label: 'VAT',
+              amount: selectedPlan.price * 0.13,
             },
           ],
           product_details: [
             {
-              identity: "1234567890",
-              name: "Khalti logo",
-              total_price: 1300,
+              identity: `${Date.now()}-${Math.floor(Math.random() * 10000)}`,
+              name: selectedPlan.name,
+              total_price: selectedPlan.price,
               quantity: 1,
-              unit_price: 1300,
+              unit_price: selectedPlan.price,
             },
           ],
         }),
         {
           headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: "Key fd0bbb0969ca474ca644b9d75e3a0452",
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: 'key live_secret_key_453bba6dce5d44698f5c3f786b976b43',
+            // Live key: e6f37d35bec24963b691f76c8d75315e
+            // a3f9becf86874842bea79b6b4cc6e8a1
+            // Satyal Test: fd0bbb0969ca474ca644b9d75e3a0452
+            // Satyal Live: 453bba6dce5d44698f5c3f786b976b43
           },
         }
       );
+
+      khaltiRequestSuccess = true;
 
       // await emailMailer.sendEmail({
       //   email: userData.email,
@@ -93,13 +111,20 @@ router.post("/khalti", async (req, res) => {
       //   lastName: userData.lname,
       // });
       return res.status(200).json({
-        message: "success",
+        message: 'success',
         payment_url: data.payment_url,
       });
     }
   } catch (error) {
     console.log(error);
-    return res.status(503).json({ message: error.response.data.message });
+    // return res.status(503).json({ message: error.response.data.message });
+    // Undo the database action if Khalti request was not successful
+    await prisma.user.delete({
+      where: {
+        email: userData.email,
+      },
+    });
+    return res.status(503).json({ message: error.message });
   } finally {
     async () => {
       await prisma.$disconnect();
@@ -107,4 +132,141 @@ router.post("/khalti", async (req, res) => {
   }
 });
 
+// router.post('/khalti', async (req, res) => {
+//   const { userData, payment, selectedPlan, userType } = req.body;
+//   console.log("This is user's data:", userData);
+//   console.log("This is plan's data:", selectedPlan);
+//   console.log("This is user's type:", userType);
+
+//   let emailExist = await prisma.user.count({
+//     where: {
+//       email: userData.email,
+//     },
+//   });
+
+//   if (emailExist > 0) {
+//     return res.status(403).json({
+//       message: 'The email address is already registered !',
+//     });
+//   }
+
+//   try {
+//     const salt = genSaltSync(10);
+//     const hashedPassword = hashSync(userData.password, salt);
+//     const result = await prisma.user.create({
+//       data: {
+//         email: userData.email,
+//         firstName: userData.firstName,
+//         middleName: userData?.middleName,
+//         lastName: userData.lastName,
+//         parentName: userData.parentName,
+//         parentContactNumber: userData.parentContactNumber,
+//         password: hashedPassword,
+//         address: userData.address,
+//         city: userData.city,
+//         province: JSON.stringify(userData.state),
+//         studentClass: JSON.stringify(userData.studentClass),
+//         userContactNumber: userData.userContactNumber,
+//         userType: userType,
+//         userStatus: 'Active',
+//         kycStatus: userType === 'Teacher' ? 'Kyc Pending' : 'Not Required',
+//       },
+//     });
+
+//     if (result) {
+//       const { data } = await axios.post(
+//         'https://khalti.com/api/v2/epayment/initiate/',
+//         JSON.stringify({
+//           return_url: 'http://localhost:3000/payment/status',
+//           website_url: 'http://localhost:3000',
+//           amount: 1300,
+//           purchase_order_id: 'Ordwer01',
+//           purchase_order_name: 'Test',
+//           customer_info: {
+//             name: userData.middleName
+//               ? `${userData.firstName} ${userData.middleName} ${userData.lastName}`
+//               : `${userData.firstName} ${userData.lastName}`,
+//             email: userData.email,
+//             phone: userData.userContactNumber,
+//           },
+//           amount_breakdown: [
+//             {
+//               label: 'Mark Price',
+//               amount: selectedPlan.price,
+//             },
+//             {
+//               label: 'VAT',
+//               amount: selectedPlan.price * 0.13,
+//             },
+//           ],
+//           product_details: [
+//             {
+//               identity: `${Date.now()}-${Math.floor(Math.random() * 10000)}`,
+//               name: selectedPlan.name,
+//               total_price: selectedPlan.price,
+//               quantity: 1,
+//               unit_price: selectedPlan.price,
+//             },
+//           ],
+//         }),
+//         {
+//           headers: {
+//             Accept: 'application/json',
+//             'Content-Type': 'application/json',
+//             Authorization: 'key live_secret_key_453bba6dce5d44698f5c3f786b976b43',
+//             // Key 453bba6dce5d44698f5c3f786b976b43
+//             // Live key: e6f37d35bec24963b691f76c8d75315e
+//             // a3f9becf86874842bea79b6b4cc6e8a1
+//             // Satyal Test: fd0bbb0969ca474ca644b9d75e3a0452
+//             // Satyal Live: 453bba6dce5d44698f5c3f786b976b43
+//           },
+//         }
+//       );
+
+//       khaltiRequestSuccess = true;
+
+//       // await emailMailer.sendEmail({
+//       //   email: userData.email,
+//       //   firstName: userData.fname,
+//       //   lastName: userData.lname,
+//       // });
+//       return res.status(200).json({
+//         message: 'success',
+//         payment_url: data.payment_url,
+//         data,
+//       });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     // return res.status(503).json({ message: error.response.data.message });
+//     // Undo the database action if Khalti request was not successful
+//     await prisma.user.delete({
+//       where: {
+//         email: userData.email,
+//       },
+//     });
+//     return res.status(503).json({ message: error.message, error: error });
+//   } finally {
+//     async () => {
+//       await prisma.$disconnect();
+//     };
+//   }
+// });
+
 module.exports = router;
+
+// curl --location 'https://khalti.com/api/v2/epayment/initiate/' \
+// --header 'Authorization: key live_secret_key_453bba6dce5d44698f5c3f786b976b43' \
+// --header 'Content-Type: application/json' \
+// --data-raw '{
+// "return_url": "http://example.com/",
+// "website_url": "http://example.com/",
+// "amount": "1000",
+// "purchase_order_id": "Ordwer01",
+// "purchase_order_name": "Test",
+// "customer_info": {
+//     "name": "Test Bahadur",
+//     "email": "test@khalti.com",
+//     "phone": "9800000001"
+// }
+// }'
